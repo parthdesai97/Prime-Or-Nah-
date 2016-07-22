2773efcd8bec321247f1027713669ce788a7d763
parthdesai97 <pdesai@g.hmc.edu>
2016-07-22 16:30:47 -0700

    Initial Commit

---
+++ Prime Or Nah?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Or Nah?
+//
+//  Created by Parth Desai on 7/22/16.
+//  Copyright © 2016 Parth Desai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
